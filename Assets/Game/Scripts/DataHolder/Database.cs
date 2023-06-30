156312489c96cd37971e74e9fbf5b0f13b384346
Furkan Elmas <furkanelmas321@gmail.com>
2023-06-30 23:25:00 +0300

    Added build

--- Assets/Game/Scripts/DataHolder/Database.cs
+++ Assets/Game/Scripts/DataHolder/Database.cs
@@ -3,7 +3,6 @@ using HyperlabCase.ScriptableObjects;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
-using UnityEditorInternal;
 using UnityEngine;
 
 namespace HyperlabCase
